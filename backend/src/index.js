871fed2f626a339cc1aedd3e4e3574254cf9a380
Bhumananda Barik <bhumanandabarik@gmail.com>
2026-06-06 17:58:30 +0530

    process env set for port

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,7 +15,8 @@ app.use("/api/philosophy", philosophyRoutes);
 app.use("/api/conflict", conflictRoutes);
 app.use("/api/debate", debateRoutes);
 
-const PORT = 5000;
+//const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
