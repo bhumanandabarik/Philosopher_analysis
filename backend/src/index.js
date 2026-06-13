0dd0710e08af3bbc335744cad82a829c21f96d17
Bhumananda Barik <bhumanandabarik@gmail.com>
2026-06-14 00:12:57 +0530

    Production deployment and rate limiting

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,16 +4,34 @@ import dotenv from "dotenv";
 import philosophyRoutes from "./routes/philosophyRoutes.js";
 import conflictRoutes from "./routes/conflictRoutes.js";
 import debateRoutes from "./routes/debateRoutes.js";
+import rateLimit from "express-rate-limit";
+
+
 
 dotenv.config();
 
 const app = express();
-app.use(cors());
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 50,
+});
+
+
+//app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "https://philosopher-analysis.vercel.app"
+    ]
+  })
+);
 app.use(express.json());
 
 app.use("/api/philosophy", philosophyRoutes);
 app.use("/api/conflict", conflictRoutes);
 app.use("/api/debate", debateRoutes);
+app.use(limiter);
 
 //const PORT = 5000;
 const PORT = process.env.PORT || 5000;
